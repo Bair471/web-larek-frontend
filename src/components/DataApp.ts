aa24d76ce596074b17245023d2662588d044ff30
Bair471 <bair.shirnen@gmail.com>
2025-03-31 21:52:37 +0800

    Commit6

---
+++ src/components/DataApp.ts
@@ -0,0 +1,101 @@
+import { IBasket, IProduct, PaymentMethod, TOrder } from '../types';
+import { EMAIL_REGEXP, TEL_REGEXP } from '../utils/constants';
+import { IEvents } from './base/events';
+
+export class AppData {
+	items: IProduct[] = [];
+	preview: IProduct | null = null;
+	basket: IBasket = {
+		items: [],
+		total: 0,
+	};
+	order: TOrder = {
+		email: '',
+		phone: '',
+		address: '',
+		payment: 'card',
+	};
+	formErrors: Partial<Record<keyof TOrder, string>> = {};
+
+	constructor(protected events: IEvents) {}
+
+	setItems(items: IProduct[]) {
+		this.items = items;
+		this.events.emit('items:change', this.items);
+	}
+
+	setPreview(item: IProduct) {
+		this.preview = item;
+		this.events.emit('preview:change', this.preview);
+	}
+
+	isInBasket(item: IProduct) {
+		return this.basket.items.includes(item.id);
+	}
+
+	addToBasket(item: IProduct) {
+		this.basket.items.push(item.id);
+		this.basket.total += item.price;
+		this.events.emit('basket:change', this.basket);
+	}
+
+	removeFromBasket(item: IProduct) {
+		this.basket.items = this.basket.items.filter((id) => id !== item.id);
+		this.basket.total -= item.price;
+		this.events.emit('basket:change', this.basket);
+	}
+
+	clearBasket() {
+		this.basket.items = [];
+		this.basket.total = 0;
+		this.events.emit('basket:change');
+	}
+
+	setPayment(method: PaymentMethod) {
+		this.order.payment = method;
+	}
+
+	setOrderField(field: keyof TOrder, value: string) {
+		if (field === 'payment') {
+			this.setPayment(value as PaymentMethod);
+		} else {
+			this.order[field] = value;
+		}
+	}
+
+	validateOrderForm() {
+		const errors: typeof this.formErrors = {};
+		if (!this.order.address) {
+			errors.address = 'Необходимо указать адрес';
+		}
+		this.formErrors = errors;
+		this.events.emit('orderFormErrors:change', this.formErrors);
+		return Object.keys(errors).length === 0;
+	}
+
+	validateContactsForm() {
+		const errors: typeof this.formErrors = {};
+		if (!this.order.email) {
+			errors.email = 'Необходимо указать email';
+		} else if (!EMAIL_REGEXP.test(this.order.email)) {
+			errors.email = 'Неправильно указан email';
+		}
+		if (!this.order.phone) {
+			errors.phone = 'Необходимо указать телефон';
+		} else if (!TEL_REGEXP.test(this.order.phone)) {
+			errors.phone = 'Неправильно указан телефон';
+		}
+		this.formErrors = errors;
+		this.events.emit('contactsFormErrors:change', this.formErrors);
+		return Object.keys(errors).length === 0;
+	}
+
+	clearOrder() {
+		this.order = {
+			email: '',
+			phone: '',
+			address: '',
+			payment: 'card',
+		};
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,43 @@
 import './scss/styles.scss';
 
+import { AppData } from './components/DataApp';
+import { Card } from './components/Card';
+import { OrderForm } from './components/Order';
+import { Page } from './components/Page';
+import { WebLarekAPI } from './components/LarekApi';
+import { EventEmitter } from './components/base/events';
+import { Basket } from './components/Basket';
+import { Modal } from './components/common/Modal';
+import { Success } from './components/Success';
+import './scss/styles.scss';
+import { IProduct, TOrder } from './types';
+import { API_URL, CDN_URL } from './utils/constants';
+import { cloneTemplate, ensureElement } from './utils/utils';
+import { ContactsForm } from './components/Contacts';
+
+const api = new WebLarekAPI(CDN_URL, API_URL);
+
+// Все шаблоны
+const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
+const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
+const cardBasketTemplate = ensureElement<HTMLTemplateElement>('#card-basket');
+const modalCardTemplate =
+	ensureElement<HTMLTemplateElement>('#modal-container');
+const orderTemplate = ensureElement<HTMLTemplateElement>('#order');
+const contactsTemplate = ensureElement<HTMLTemplateElement>('#contacts');
+const successTemplate = ensureElement<HTMLTemplateElement>('#success');
+
+const events = new EventEmitter();
+
+// Модель данных приложения
+const appData = new AppData(events);
+
+// Глобальные контейнеры
+const modal = new Modal(modalCardTemplate, events);
+const page = new Page(document.body, events);
+const basket = new Basket(events);
+const orderForm = new OrderForm(cloneTemplate(orderTemplate), events);
+const contactsForm = new ContactsForm(cloneTemplate(contactsTemplate), events);
+const success = new Success(cloneTemplate(successTemplate), events, {
+	onClick: () => modal.close(),
+});
